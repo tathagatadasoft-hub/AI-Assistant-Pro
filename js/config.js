923bb9a550c8389a381a4671b4387ce265267346
tathagatadasoft-hub <tathagata.dasoft@gmail.com>
2026-07-11 13:45:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
+// ===============================
+// Google Gemini Configuration
+// ===============================
 
+// Paste your API key below
+const API_KEY = "AQ.Ab8RN6IwNkEpsQOUrKk9rf5WHYiut5WyqVfv0FDroKCr_qXdHA";
+
+// Gemini Model
+const MODEL = "gemini-2.5-flash";
+
+// API URL
+const API_URL =
+`https://generativelanguage.googleapis.com/v1beta/models/${MODEL}:generateContent?key=${API_KEY}`;
